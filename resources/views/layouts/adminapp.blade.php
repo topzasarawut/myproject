f0734b94713d449d3841e657854f459e4a2e6c8f
topzasarawut <topzaneverdie@gmail.com>
2022-05-24 15:49:22 +0700

    edit view adminapp.blade

--- resources/views/layouts/adminapp.blade.php
+++ resources/views/layouts/adminapp.blade.php
@@ -26,7 +26,7 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <a href="#"> | ข้อมูลนักเรียน</a>
+                <a href="#"> | เพิ่มข้อมูลนักเรียน</a>
                 <a href="{{ route('school.create') }}"> | เพิ่มข้อมูลโรงเรียน </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
